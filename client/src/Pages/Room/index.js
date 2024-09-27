e9ef0de1dcd0fa4aa4f6691fbc92350c85261c97
ZENDER\AC110416 <aaryan.chouksey@gamesglobal.com>
2024-09-27 11:28:06 +0530

    unread message number ui change

--- client/src/Pages/Room/index.js
+++ client/src/Pages/Room/index.js
@@ -469,7 +469,7 @@ const Room = () => {
             >
               <img src={chatBubble} alt="Chat" className="w-10 h-10" />
               {unreadMessages > 0 && (
-                <span className="absolute -top-1 -right-1 bg-red-500 text-white rounded-full px-1 text-xs">
+                <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full px-2 py-0.5 text-sm">
                   {unreadMessages}
                 </span>
               )}
